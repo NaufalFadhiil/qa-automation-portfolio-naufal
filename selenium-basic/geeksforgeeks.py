9a4188895cd6fb8779b163bb00045e1352f0ab85
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2026-03-03 15:29:19 +0700

    feat: add new tab handling with explicit wait and window switching after result click

--- selenium-basic/geeksforgeeks.py
+++ selenium-basic/geeksforgeeks.py
@@ -15,11 +15,13 @@ driver.get("https://www.geeksforgeeks.org/")
 wait = WebDriverWait(driver, 10)
 home_url = driver.current_url
 
+main_tab = driver.current_window_handle
+
 time.sleep(3)
 
 # Locate the input field for search text and enter
 search = driver.find_element(By.CLASS_NAME, "HomePageSearchContainer_homePageSearchContainer_container_input__1LS0r")
-search.send_keys("Data Structure")
+search.send_keys("Python")
 search.send_keys(Keys.ENTER)
 
 time.sleep(3)
@@ -28,8 +30,17 @@ time.sleep(3)
 wait.until(lambda d: d.current_url != home_url)
 driver.get(home_url)
 
-# Click View More
-driver.find_element(By.XPATH, '//*[@id="comp"]/div[2]/div[2]/div/div[2]/a[2]/button').click()
+time.sleep(3)
+
+# Click on the first link
+driver.find_element(By.XPATH, '//*[@id="comp"]/div[2]/div[2]/div/div[2]/a[5]/button').click()
+
+wait.until(lambda d: len(d.window_handles) > 1)
+all_tabs = driver.window_handles
+driver.switch_to.window(all_tabs[-1])
+
+time.sleep(3)
+driver.switch_to.window(main_tab)
 
 # QUIT
 time.sleep(3)
