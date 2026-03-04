a13bfee5c263913aa8eb3418e92ed8f96f76d16b
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2026-03-04 23:59:28 +0700

    Add explicit wait and alert handling to automation script

--- selenium-basic/alerts.py
+++ selenium-basic/alerts.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 import time
 
 # Tidak auto close browser
@@ -8,7 +11,7 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options = options)
 driver.get("https://demoqa.com/alerts")
 
-# Normal alert
+# 1. Normal alert
 driver.find_element(By.ID, "alertButton").click()
 time.sleep(2)
 
@@ -16,16 +19,21 @@ driver.switch_to.alert.accept()
 time.sleep(3)
 
 
-# Button alert 5 detik
+# 2. Button alert 5 detik
 driver.find_element(By.ID, "timerAlertButton").click()
-time.sleep(5)
 
-time.sleep(2)
-driver.switch_to.alert.accept()
+#
+try:
+    WebDriverWait(driver, 10).until(EC.alert_is_present())
+    driver.switch_to.alert.accept()
+    print("Alert berhasil muncul")
+except TimeoutException:
+    print("Tidak ada alert")
+
 time.sleep(3)
 
 
-# Cpmfirm alert
+# 3. Comfirm alert
 driver.find_element(By.ID, "confirmButton").click()
 time.sleep(2)
 
@@ -34,7 +42,7 @@ driver.switch_to.alert.dismiss()
 time.sleep(3)
 
 
-# Prompt alert
+# 4. Prompt alert
 driver.find_element(By.ID, "promtButton").click()
 time.sleep(2)
 
