112c7eb8fb424712c40fe93c6369786c6b5f0c81
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2026-03-23 22:03:39 +0700

    test(search): add automation search for select destination

--- selenium-basic/booking_com/booking/booking.py
+++ selenium-basic/booking_com/booking/booking.py
@@ -19,30 +19,28 @@ class Booking(webdriver.Chrome):
         self.get("https://www.booking.com")
 
     def exit_modals(self):
-        try:
-            exit_modals = WebDriverWait(self, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[aria-label="Dismiss sign-in info."]')))
-            print('pop up modal muncul')
-            exit_modals.click()
-            print('pop up modal berhasil ditutup')
-        except Exception as e:
-            print(f"Error: {e}")
-            pass
+        exit_modals = WebDriverWait(self, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[aria-label="Dismiss sign-in info."]')
+        ))
+        exit_modals.click()
 
     def change_currency(self, currency = "IDR"):
-        try:
-            change_currency = self.find_element(
-            By.CSS_SELECTOR,
-            '[data-testid="header-currency-picker-trigger"]'
-            )
-            change_currency.click()
+        change_currency = self.find_element(By.CSS_SELECTOR, '[data-testid="header-currency-picker-trigger"]')
+        change_currency.click()
 
-            time.sleep(2),
+        time.sleep(2),
             
-            quit_currency = WebDriverWait(self, 10).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="selection-modal-close"]'))) 
-            quit_currency.click()
-       
-        except:
-            TimeoutError
-            pass
+        quit_currency = WebDriverWait(self, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="selection-modal-close"]')
+        )) 
+        quit_currency.click()
+
+    def select_place_to_go(self, place_to_go):
+        search_field = self.find_element(By.NAME, 'ss')
+        search_field.clear() 
+        search_field.send_keys(place_to_go)
+
+        first_result = WebDriverWait(self, 5).until(
+            EC.element_to_be_clickable((By.ID, 'autocomplete-result-0')
+        )) 
+        first_result.click()
--- selenium-basic/booking_com/run.py
+++ selenium-basic/booking_com/run.py
@@ -4,6 +4,7 @@ bot = Booking()
 bot.web_page()
 bot.exit_modals()
 bot.change_currency(currency="IDR")
+bot.select_place_to_go('Jakarta')
 
 input("Press enter to close...")
 bot.quit()
