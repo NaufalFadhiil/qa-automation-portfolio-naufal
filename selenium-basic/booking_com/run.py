ebb234b5fe1422e33503679b6bdafde9abcb3882
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2026-04-04 09:50:51 +0700

    feat(cli): improve input hints

--- selenium-basic/booking_com/run.py
+++ selenium-basic/booking_com/run.py
@@ -5,20 +5,20 @@ bot.web_page()
 bot.exit_modals()
 bot.change_currency(currency="IDR")
 
-destination = input("Where your destination to go?") # TODO: Clue untuk mengisi tiap input
-check_in = input("What is your check IN date?")
-check_out = input("What is your check OUT date?")
+destination = input("Where your destination to go? (e.g. Jakarta): ")
+check_in = input("What is your check IN date?" + " (YYYY-MM-DD): ")
+check_out = input("What is your check OUT date?" + " (YYYY-MM-DD): ")
 
-adults = int(input("How many adults?"))
-children = int(input("How many children?"))
+adults = int(input("How many adults? "))
+children = int(input("How many children? "))
 
 children_age = []
 for i in range(children):
-    age = int(input(f"What is child {i+1} age?"))
+    age = int(input(f"What is child {i+1} age? "))
     children_age.append(age)
     # TODO: Throw exception for children age > 17 < 18
 
-rooms = int(input("How many rooms?"))
+rooms = int(input("How many rooms? "))
 
 bot.select_place_to_go(destination)
 bot.select_dates(check_in_date=check_in, check_out_date=check_out)
@@ -31,7 +31,8 @@ bot.select_guest(
 bot.pets_toggle()
 bot.done_button()
 bot.click_search()
-bot.exit_modals()
+
+# bot.exit_modals() # Un-Comment if there is a popup modal 
 
 bot.apply_filtration()
 
