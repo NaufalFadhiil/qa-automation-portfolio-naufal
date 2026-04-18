c660ac2c26e782b49fdbd785c479b11eed65ea85
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2026-04-18 13:42:35 +0700

    refactor: use pytest parameterize for rectangle tests

--- selenium-basic/pytest/test_rectangle.py
+++ selenium-basic/pytest/test_rectangle.py
@@ -4,6 +4,8 @@ def calculate_rectangle(width, height):
     area = width * height
     return area
 
-def test_add_rectangle():
-    result = calculate_rectangle(5, 15)
-    assert result == 75
+@pytest.mark.parametrize("width, height, expected_result", [(5, 15, 75), (10, 20, 200), (20, 30, 600), (25, 35, 875)])
+def test_add_rectangle(width,height, expected_result):
+    result = calculate_rectangle(width, height)
+    print(result)
+    assert result == expected_result
