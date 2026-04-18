b292bbef29ed744ca59e83207e3ef83694b98717
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2026-04-18 17:19:49 +0700

    feat: add fixture for simple math

--- selenium-basic/pytest/test_rectangle.py
+++ selenium-basic/pytest/test_rectangle.py
@@ -9,3 +9,23 @@ def test_add_rectangle(width,height, expected_result):
     result = calculate_rectangle(width, height)
     print(result)
     assert result == expected_result
+
+@pytest.fixture()
+def setup():
+    global num1, num2
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    yield
+    print("\nOutput:", output)
+
+def test_addition(setup):
+    global output
+    output = num1 + num2
+
+def test_subtraction(setup):
+    global output
+    output = num1 - num2
+
+def test_multiplication(setup):
+    global output
+    output = num1 * num2
