6ee03359b20ad7b5841c1be3ab1b220643febe16
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2026-04-23 23:13:11 +0700

    feat: automate input username and password with explicit wait

--- selenium-basic/orangehrm_test/login.py
+++ selenium-basic/orangehrm_test/login.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
@@ -11,6 +11,17 @@ driver = webdriver.Chrome(options=options)
 
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 driver.maximize_window()
+wait = WebDriverWait(driver, 10)
 
-time.sleep(2)
+username_input = wait.until(
+    EC.visibility_of_element_located((By.NAME, "username"))
+)
+username_input.send_keys("Admin")
+
+password_input = wait.until(
+    EC.visibility_of_element_located((By.NAME, "password"))
+)
+password_input.send_keys("admin123")
+
+time.sleep(1)
 driver.quit()
